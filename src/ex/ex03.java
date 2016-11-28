c0283803f26decd572ad1f3fd2d290c4382e2016
battlesushi <105021030@live.asia.edu.tw>
2016-11-28 18:06:44 +0800

    ex

--- src/ex/ex03.java
+++ src/ex/ex03.java
@@ -1,15 +1,33 @@
 ﻿package ex;
+import java.util.*;
 /*
  * Topic: 讓使用者輸入一正整數 n，輸出 n 的所有正因數，並判斷該整數是否為質數
  * Date: 2016/11/28
- * Author: 1050210XX 周永振老師
+ * Author: 105021030 張廷毓
  */
-
 public class ex03 {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc=new Scanner(System.in);
+		System.out.print("輸入(>=2)的值:");
+		int n=sc.nextInt();
+		int x,count=0;
+		System.out.print(1+"   ");
+		for(int i=2;i<=n;i++){
+			if(n%i==0)
+				System.out.print(i+"   ");
+		}
+		x=n/2;
+		if(x%2 == 0 && x != 2) 
+			count++;
+		if(x==1)
+			count++;
+			for(int i=2; i<=x; i++){ 
+				if(n%i==0)
+					count++;
+			}
+		if(count==0)
+			System.out.print("此為質數");
+		
 	}
 
 }
